38d8d6948b3a4231321cf578b7788a3f22f17277
Shubham Kumar <codershubham2k1@gmail.com>
2022-01-12 17:20:56 +0530

    Changes

---
+++ src/com/company/buySellStocks.java
@@ -0,0 +1,17 @@
+package com.company;
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+public class buySellStocks {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
